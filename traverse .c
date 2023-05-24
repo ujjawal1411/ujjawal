3f1e23219fec22a535583272fe36666bc8b7ce29
ujjawal singh charak <120174365+ujjawal1411@users.noreply.github.com>
2023-05-24 21:36:57 +0530

    Add files via upload

---
+++ traverse .c
@@ -0,0 +1,14 @@
+// C program to implement a list using array and develop function to perform traversal operation
+//header files
+#include <stdio.h>
+//main function
+void main() {
+    //  list input
+int Arr[8] = {98, 67, 45, 78, 56, 34, 17, 42};
+int i;
+//input prompt
+printf("Elements of the array are:\n");
+for(i = 0; i<8; i++) {
+printf("Arr[%d] = %d,  ", i, Arr[i]);
+}
+}
